284e5ed01b3158930dff2412afaf0c3005878292
liutao <215711645@qq.com>
2017-04-21 10:12:38 +0800

    自定义多条跑马效果

---
+++ app/src/main/java/com/base/resultset/ui/textview/widget/MarqureeTextView.java
@@ -0,0 +1,33 @@
+package com.base.resultset.ui.textview.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 自定义跑马灯效果 Textview
+ * Created by Administrator on 2017/4/20.
+ */
+
+public class MarqureeTextView extends TextView {
+    public MarqureeTextView(Context context) {
+        super(context);
+    }
+
+    public MarqureeTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqureeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MarqureeTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
