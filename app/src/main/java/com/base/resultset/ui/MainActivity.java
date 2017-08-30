6b56cb5b95471146f239ae8c4514407858fb5cc4
liutao <215711645@qq.com>
2017-08-30 09:39:29 +0800

    修改内容

--- app/src/main/java/com/base/resultset/ui/MainActivity.java
+++ app/src/main/java/com/base/resultset/ui/MainActivity.java
@@ -1,13 +1,17 @@
 package com.base.resultset.ui;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.base.resultset.R;
 import com.base.resultset.base.BaseActivity;
 import com.base.resultset.ui.imageview.ImageActivity;
+import com.base.resultset.ui.newapp.startmodule.broadcast.HeadsetReceiver;
 import com.base.resultset.ui.textview.TextActivity;
 import com.base.resultset.utils.Common;
 
@@ -24,6 +28,12 @@ public class MainActivity extends BaseActivity {
             butt.setOnClickListener(this);
         }
         setTitleBarVisible(false);
+        //注册广播
+        HeadsetReceiver noisyAudioStreamReceiver = new HeadsetReceiver();
+        IntentFilter filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
+        registerReceiver(noisyAudioStreamReceiver, filter);
+
     }
 
     @Override
@@ -36,11 +46,18 @@ public class MainActivity extends BaseActivity {
                 Common.ActivitySkip(this, ImageActivity.class);
                 break;
             case R.id.button:
-                Common.ActivitySkip(this, ImageActivity.class);
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.liutao.help.personalbutler");
+                if (intent != null) {
+                    startActivity(intent);
+                } else {
+                    // 没有安装要跳转的app应用，提醒一下
+                    Toast.makeText(getApplicationContext(), "哟，赶紧下载安装这个APP吧", Toast.LENGTH_LONG).show();
+                }
                 break;
         }
 
     }
+
 
 
 }
